f3794055e5caf4fbd9731a3c723111cae2ab98a9
Mitsuhiro GOTA <mitsugogo@gmail.com>
2023-07-31 22:48:24 +0900

    fix typo

--- script_google.js
+++ script_google.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     // favicon
-    waitForSelectors("a[href*='twitter.com'] img.XNo5Ab, a[href*='twitter.com'] img.no69gc").then(changeFavion);
-    waitForSelectors("a[href^='https://x.com'] img.XNo5Ab, a[href^='https://x.com'] img.no69gc").then(changeFavion);
-    waitForSelectors("a[href^='https://t.co/'] img.XNo5Ab, a[href^='https://t.co/'] img.no69gc").then(changeFavion);
+    waitForSelectors("a[href*='twitter.com'] img.XNo5Ab, a[href*='twitter.com'] img.no69gc").then(changeFavicon);
+    waitForSelectors("a[href^='https://x.com'] img.XNo5Ab, a[href^='https://x.com'] img.no69gc").then(changeFavicon);
+    waitForSelectors("a[href^='https://t.co/'] img.XNo5Ab, a[href^='https://t.co/'] img.no69gc").then(changeFavicon);
 });
 
-function changeFavion(imgs) {
+function changeFavicon(imgs) {
     if (!imgs) return;
     imgs.forEach(element => {
         element.src = TWITTER_FAVICON;
